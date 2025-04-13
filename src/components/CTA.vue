19710b53e6a5e74460d69b7fff45bf356c4ab53e
Marco Zaccaria <marcozak17@gmail.com>
2025-04-13 21:45:35 +0200

    fix CTA

--- src/components/CTA.vue
+++ src/components/CTA.vue
@@ -1,18 +1,24 @@
 <script setup>
 import translations from '@/translations/translations.json'
-import  { getIcon } from '@/utils'
+import { getIcon } from '@/utils'
 </script>
 
 <template>
-   <button 
-        class="flex justify-center items-center w-full px-3 rounded-full bg-gray-dark border-blue border-2 h-12"
-        @click="$emit('ctaClicked')"
-    >
-        <div class=" h-[40px] w-[40px] flex justify-center items-center">
-            <component :is="getIcon(translations.CTA.icon)"/>
-        </div>
-        <p class="font-PeugeotNewBold text-xs text-white">{{ translations.CTA.label }}</p>
-    </button>
+  <button 
+    class="flex items-center justify-center w-full px-3 rounded-full bg-gray-dark border-blue border-2 h-12"
+    @click="$emit('ctaClicked')"
+  >
+    <!-- Icona -->
+    <div class="w-[24px] flex justify-center items-center">
+      <component :is="getIcon(translations.CTA.icon)" class="h-[20px] w-[20px]" />
+    </div>
+
+    <!-- Testo -->
+    <p class="font-PeugeotNewBold text-xs text-white ml-2 text-center">
+      {{ translations.CTA.label }}
+    </p>
+  </button>
 </template>
+
 
 
